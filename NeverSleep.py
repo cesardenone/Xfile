88d4be0d09e890391c6db1791748598ae4fa2d02
cesardenone <cesar.denone@outlook.com>
2022-08-26 14:26:24 -0300

    Update NeverSleep.py

--- NeverSleep.py
+++ NeverSleep.py
@@ -1,11 +1,14 @@
+from operator import truediv
 import pyautogui, random, time
+from tkinter import *
 print('Press Ctrl-C to quit.')
 pyautogui.FAILSAFE = False
 
+mouse=True
 
-try:
-    while True:
-        pyautogui.hotkey('Win','d')
+def mover():
+    pyautogui.hotkey('Win', 'd')
+    while mouse==True:
         posicao1 = random.randint(1,1000)
         posicao2 = random.randint(1,1000)
         tempo = random.randint(2,10)
@@ -13,5 +16,18 @@ try:
         pyautogui.moveTo(posicao1, posicao2, duration=arrastar)
         pyautogui.leftClick()
         time.sleep(tempo)
-except KeyboardInterrupt:
-    print('\n')
+    return
+
+def parar():
+    return mouse==False
+
+master = Tk()
+master.geometry("180x160")
+
+master.configure(bg='light grey')
+
+Button(master, text="Mover Mouse", command=mover).place(x=40, y=30)
+Button(master, text="Parar Mouse", command=parar).place(x=40, y=80)
+
+mainloop()
+
