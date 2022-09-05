51f759e3f015b5575b6ed6999a42c55b2de772dc
Cesar Denone <cesar.denone@outlook.com>
2022-09-05 20:05:11 -0300

    Update NeverSleep.py

--- NeverSleep.py
+++ NeverSleep.py
@@ -1,33 +1,29 @@
-from operator import truediv
 import pyautogui, random, time
 from tkinter import *
-print('Press Ctrl-C to quit.')
 pyautogui.FAILSAFE = False
 
-mouse=True
+mouse=False
 
-def mover():
-    pyautogui.hotkey('Win', 'd')
+def mover(mouse):
+    #pyautogui.hotkey('Win', 'd')#
     while mouse==True:
         posicao1 = random.randint(1,1000)
         posicao2 = random.randint(1,1000)
         tempo = random.randint(2,10)
         arrastar = random.randint(1,5)
         pyautogui.moveTo(posicao1, posicao2, duration=arrastar)
-        pyautogui.leftClick()
+        #pyautogui.leftClick()#
         time.sleep(tempo)
     return
 
-def parar():
-    return mouse==False
+app = Tk()
+app.title("Never Sleep")
+app.geometry("250x150")
+app.configure(background="#000")
 
-master = Tk()
-master.geometry("180x160")
+text1=Button(app, text="Mouse Moving", activebackground="#999", command=mover(True))
+text1.place(x=80, y=40, width=100, height=30)
+text2=Label(app, text="Ctrl+C to stop", background="#000", foreground="#999")
+text2.place(x=80, y=80, width=100, height=30)
 
-master.configure(bg='light grey')
-
-Button(master, text="Mover Mouse", command=mover).place(x=40, y=30)
-Button(master, text="Parar Mouse", command=parar).place(x=40, y=80)
-
-mainloop()
-
+app.mainloop()
